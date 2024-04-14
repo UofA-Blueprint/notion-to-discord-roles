7ead821fe9b78a8ddb09b5a8bf02ab29efa7746a
JanukanS <28988453+JanukanS@users.noreply.github.com>
2024-04-14 09:40:02 -0600

    Added setRole command to assign a discord role based on notion data
    Modified fetchNotionDatabase from notionFetch.js to account for api pagination

---
+++ commands/utility/setRole.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('discord.js');
+const {fetchNotionDatabase, parseResults,collectHandles}= require("../../services/notionFetch")
+const {notion_page_id, notion_api_key} = require("../../config.json")
+
+const {Client} = require("@notionhq/client");
+const notion = new Client({ auth: notion_api_key })
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('setrole')
+        .setDescription('Gives a user a role based on notion data')
+        .addUserOption(option => option.setName('user').setDescription('User to give role to').setRequired(true)),
+    async execute(interaction) {
+        //const role = interaction.options.getRole('role');
+        const user = interaction.options.getMember('user');
+        const guild = interaction.guild;
+        const username = user.user.username
+
+        let ndata = await fetchNotionDatabase(notion, notion_page_id)
+        let roledata = await collectHandles(ndata.results)
+        const role = guild.roles.cache.find(role => role.name === roledata[username][1]);
+        let membername = roledata[username][0]
+        if (!role) {
+            await interaction.reply(`Role not found.`);
+            return;
+        }
+        await user.roles.add(role);
+
+        await interaction.reply(`${username} (${membername }) has been assigned to ${role}`);
+    },
+};
--- services/notionFetch.js
+++ services/notionFetch.js
@@ -1,20 +1,49 @@
 async function fetchNotionDatabase(notionClient, databaseId) {
     //get data from notion as a json respons to api query
+    let returnResponse = {"results": []}
     try {
-        const response = await notionClient.databases.query({
+        let response = await notionClient.databases.query({
             database_id: databaseId,
         });
-        return response;
+        returnResponse.results.push(...response.results)
+        while (response.has_more){
+            response = await notionClient.databases.query({
+                database_id: databaseId,
+                start_cursor: response.next_cursor
+            });
+            returnResponse.results.push(...response.results)
+        }
+
+        return returnResponse;
     } catch (error) {
         console.error(error.body);
         throw new Error('Failed to fetch Notion database');
     }
 }
 
+function collectHandles(res){
+    let handles = {}
+    let handleRichText = ""
+    for (row of res){
+        handleRichText = row.properties["Discord Handle"].rich_text
+        if (handleRichText.length > 0) {
+            handles[handleRichText[0].plain_text] = [row.properties.Name.title[0].plain_text,
+                row.properties['Current Role'].select.name]
+
+        }
+
+
+    }
+    return handles
+}
+
 function parseResults(res){
     //Select 10 random members from the database and print name + position
     let name = "";
     let pos = "";
+    let discHandle = "";
+    let discHandleRichText = "";
+
     const numMembers = res.length;
 
     let output = `There are ${numMembers} members in the directory\n`;
@@ -25,12 +54,19 @@ function parseResults(res){
         rowInd = Math.floor(Math.random()*numMembers)
         name = res[rowInd].properties.Name.title[0].plain_text;
         pos = res[rowInd].properties['Current Role'].select.name;
-        output = output.concat(`${name} (${pos})\n`)
+        discHandleRichText = res[rowInd].properties["Discord Handle"].rich_text
+        if (discHandleRichText.length > 0) {
+            discHandle = discHandleRichText[0].plain_text
+        } else {
+            discHandle = "No Discord Handle Found"
+        }
+        output = output.concat(`${name} (${pos}) (${discHandle})\n`)
     }
     return output
 }
 
 module.exports = {
     fetchNotionDatabase,
-    parseResults
+    parseResults,
+    collectHandles
 }
