9c171520632692c9268fce1a7883408affcf08b5
eggmangrg <aagaman@ualberta.ca>
2024-11-03 10:36:55 -0700

    setEveryone timeout chaange

--- commands/utility/setEveryone.js
+++ commands/utility/setEveryone.js
@@ -12,25 +12,25 @@ module.exports = {
         const { options, guild } = interaction;
         const members = await guild.members.fetch();
         const role = options.getRole('role');
-        if (!interaction.member.permissions.has(PermissionsBitField.FLAGS.ADMINISTRATOR))
-            return await interaction.reply({ content: 'You do not have permission to use this command', ephemeral: true });
-        else {
-            await interaction.reply({ content: `Setting role ${role.name} to everyone` });
-            let num = 0;
-            setTimeout(() => {
-                members.forEach(async m => {
-                    m.roles.add(role).catch(err => {
-                        return;
-                    });
-                    num++;
+        await interaction.deferReply();
+        await interaction.editReply({ content: `Setting role ${role.name} to everyone` });
+        let num = 0;
+        
+        setTimeout(() => {
+            members.forEach(async m => {
+                m.roles.add(role).catch(err => {
+                    return;
+                });
+                num++;
 
-                    const embed = new EmbedBuilder()
-                        .setColor('Blue')
-                        .setDescription(`❗${num} members have ${role.name} `)
-                    await interaction.editReply({ content: '', embeds: [embed] });
-                })
-            }, 100)
-        }
+                const embed = new EmbedBuilder()
+                    .setColor('Blue')
+                    .setDescription(`❗Giving ${num} members ${role.name} `)
+                
+                await interaction.editReply({ content: '', embeds: [embed] });
+            })
+        }, 100)
+
     }
 }
 
